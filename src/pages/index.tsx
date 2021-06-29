7d654b1b4322c53fc6108c1b9197bb9d19b5b982
Elvis Felix <elvisjaspion@gmal.com>
2021-06-29 04:04:18 -0300

    create an page home

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+     <main>
+       <section>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the </h1>
+       </section>
+
+       <img src="/images/avatar.svg" alt="Girl coding" />
+     </main>
+    </>
+  )
+}
